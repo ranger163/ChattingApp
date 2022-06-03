4353188935c9b16af468e490d3dcc3d2375cb6e4
ranger163 <ahmed.nassar163@gmail.com>
2022-06-04 00:00:25 +0300

    Update: - Adding username validation.

--- app/src/main/java/me/inassar/demos/socialapp/presentation/common/TextFieldValidatorState.kt
+++ app/src/main/java/me/inassar/demos/socialapp/presentation/common/TextFieldValidatorState.kt
@@ -5,7 +5,11 @@ package me.inassar.demos.socialapp.presentation.common
 import android.util.Patterns
 import java.util.regex.Pattern
 
+class Username :
+    TextFieldState(validator = ::isUsernameValid, errorMessage = ::usernameErrorMessage)
+
 class EmailState : TextFieldState(validator = ::isEmailValid, errorMessage = ::emailErrorMessage)
+
 class PasswordState :
     TextFieldState(validator = ::isPasswordValid, errorMessage = ::passwordErrorMessage)
 
@@ -19,4 +23,10 @@ private fun isPasswordValid(password: String): Boolean =
     password.length >= 8
 
 private fun passwordErrorMessage(password: String) =
-    if (password.isEmpty()) "Email field is required." else "Password should be at least 8 characters."
+    if (password.isEmpty()) "Password field is required." else "Password should be at least 8 characters."
+
+private fun isUsernameValid(username: String): Boolean =
+    username.length >= 3
+
+private fun usernameErrorMessage(username: String) =
+    if (username.isEmpty()) "Username field is required." else "Username should be at least 3 characters."
