37eef18c5b6ec54e2df1bd22b31edc85ef2f7c7e
ranger163 <ahmed.nassar163@gmail.com>
2022-06-03 23:47:34 +0300

    Update: - Implementing signup call into auth repository

--- app/src/main/java/me/inassar/demos/socialapp/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/me/inassar/demos/socialapp/data/repository/AuthRepositoryImpl.kt
@@ -5,10 +5,20 @@ import kotlinx.coroutines.flow.flow
 import me.inassar.demos.socialapp.common.ResponseResource
 import me.inassar.demos.socialapp.data.remote.dto.login.request.LoginRequestDto
 import me.inassar.demos.socialapp.data.remote.dto.login.response.LoginResponseDto
+import me.inassar.demos.socialapp.data.remote.dto.signup.request.SignupRequestDto
+import me.inassar.demos.socialapp.data.remote.dto.signup.response.SignupResponseDto
 import me.inassar.demos.socialapp.data.remote.source.AuthRemote
 import me.inassar.demos.socialapp.domain.repository.AuthRepository
 
 class AuthRepositoryImpl(private val remote: AuthRemote) : AuthRepository {
+
+    override suspend fun signup(requestDto: SignupRequestDto): Flow<ResponseResource<SignupResponseDto>> =
+        flow {
+            when (val response = remote.signup(requestDto)) {
+                is ResponseResource.Error -> emit(ResponseResource.error(response.errorMessage))
+                is ResponseResource.Success -> emit(ResponseResource.success(response.data))
+            }
+        }
 
     override suspend fun login(request: LoginRequestDto): Flow<ResponseResource<LoginResponseDto>> =
         flow {
--- app/src/main/java/me/inassar/demos/socialapp/domain/repository/AuthRepository.kt
+++ app/src/main/java/me/inassar/demos/socialapp/domain/repository/AuthRepository.kt
@@ -4,7 +4,10 @@ import kotlinx.coroutines.flow.Flow
 import me.inassar.demos.socialapp.common.ResponseResource
 import me.inassar.demos.socialapp.data.remote.dto.login.request.LoginRequestDto
 import me.inassar.demos.socialapp.data.remote.dto.login.response.LoginResponseDto
+import me.inassar.demos.socialapp.data.remote.dto.signup.request.SignupRequestDto
+import me.inassar.demos.socialapp.data.remote.dto.signup.response.SignupResponseDto
 
 interface AuthRepository {
+    suspend fun signup(requestDto: SignupRequestDto): Flow<ResponseResource<SignupResponseDto>>
     suspend fun login(request: LoginRequestDto): Flow<ResponseResource<LoginResponseDto>>
 }
